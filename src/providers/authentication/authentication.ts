38e948d398de8b6d660985d4553de985cc6888aa
sofvindex <grannyts@gmail.com>
2018-09-11 11:04:20 +0200

    add login, register, logut function to auth provider

--- src/providers/authentication/authentication.ts
+++ src/providers/authentication/authentication.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { User } from '../../models/user/user';
+import { Observable } from 'rxjs/Observable';
 
 /*
   Generated class for the AuthenticationProvider provider.
@@ -11,8 +12,27 @@ import { User } from '../../models/user/user';
 @Injectable()
 export class AuthenticationProvider {
 
+	private user: Observable<firebase.User>;
+	private userDetails: firebase.User = null;
+
   constructor(private angularAuth: AngularFireAuth) {
     console.log('Hello AuthenticationProvider Provider');
+     this.user = angularAuth.authState;
+        this.user.subscribe(
+            (user) => {
+                if (user) {
+                    this.userDetails = user;
+                    console.log("From service constructor subscribe:", this.userDetails);
+                }
+                else {
+                    this.userDetails = null;
+                }
+            }
+        );
+  }
+
+  getAuthState(){
+        return this.angularAuth.authState;
   }
 
 }
