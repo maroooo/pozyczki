9faeb2a059fdf523215d724b2e8dc27f260f9e7b
wujek1 <lukasz.luty@gmail.com>
2018-09-11 13:33:34 +0200

    add imports to provider

--- src/providers/items/items.ts
+++ src/providers/items/items.ts
@@ -1,5 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+//import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument  } from 'angularfire2/firestore';
+import { Item } from '../../models/item/item';
 
 /*
   Generated class for the ItemsProvider provider.
@@ -10,7 +13,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ItemsProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(private fireStore: AngularFirestore) {
     console.log('Hello ItemsProvider Provider');
   }
 
