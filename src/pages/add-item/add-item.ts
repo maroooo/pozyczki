490331831050342e6b6c26ef648d78a7385bcccb
wujek1 <lukasz.luty@gmail.com>
2018-09-11 13:23:46 +0200

    add new item form with validation

--- src/pages/add-item/add-item.ts
+++ src/pages/add-item/add-item.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Item } from '../../models/item/item';
+import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
 /**
  * Generated class for the AddItemPage page.
  *
@@ -15,11 +16,52 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddItemPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	addItemForm: FormGroup;
+
+  constructor(public navCtrl: NavController, 
+  						public navParams: NavParams,
+  						private formBuilder: FormBuilder) {
+  	this.addItemForm = formBuilder.group({
+      name: ['', Validators.compose([Validators.required])],
+      state: ['', Validators.compose([Validators.required])],
+      category: ['', Validators.compose([Validators.required])],
+      person: '',
+      startDate: ['', Validators.compose([Validators.required])],
+      endDate: ['', Validators.compose([Validators.required])],
+      comment: ''
+    })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddItemPage');
   }
 
+  item: Item = {
+    id: '',
+    uid: '',
+    name: '',
+    state: '',
+    category: '',
+    person: '',
+    startDate: '',
+    endDate: '',
+    comment: '',
+    image: ''
+  }
+
+  contactsList = ['Adam Nowak', 'Maciek', 'Jan Kowalski', 'Adam Małysz', 'Monika Bąk', 'Kapitan', 'Mama', 'Najlepszy Brat'];
+  categories = ['pieniądze', 'książka', 'narzędzia', 'urządzenia', 'samochód'];
+  states = ['wypożyczam', 'pożyczam'];
+
+  addItem() {
+
+  }
+
+  takePicture() {
+
+  }
+
+  getPicture() {
+
+  }
 }
