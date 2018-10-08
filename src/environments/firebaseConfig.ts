628206d96c505ce184c6bbbd066b7e5821a00748
maroooo <slosarczyk.marek@gmail.com>
2018-10-08 09:08:44 +0200

    Changed to working firebase account

--- src/environments/firebaseConfig.ts
+++ src/environments/firebaseConfig.ts
@@ -6,11 +6,11 @@
 export const environment = {
     production: false,
     firebase: {
-      apiKey: "AIzaSyDZnjqKjZPE1NCIrPGu2YAShqVr8_6hh6M",
-    	authDomain: "brrwapp.firebaseapp.com",
-    	databaseURL: "https://brrwapp.firebaseio.com",
-    	projectId: "brrwapp",
-    	storageBucket: "brrwapp.appspot.com",
-    	messagingSenderId: "572772983888"
+      apiKey: "AIzaSyDViATDhloUelL88L2ZopPcCWlkH0ylMkw",
+      authDomain: "pozyczka-d434c.firebaseapp.com",
+      databaseURL: "https://pozyczka-d434c.firebaseio.com",
+      projectId: "pozyczka-d434c",
+      storageBucket: "pozyczka-d434c.appspot.com",
+      messagingSenderId: "881910733751"
     }
 };
